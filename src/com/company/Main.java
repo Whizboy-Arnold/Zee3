85af31cfc7c77345f313da1b2c7feea53cec2bc2
warren <shigoliwarren@gmail.com>
2020-11-09 18:30:07 +0300

    on realization long will just suffice for numbers below 21 hopefully the sum still fits too. otherwise bignumber wl have to be used

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,8 @@ public class Main {
     final int STEP1=1;
     final int STEP2=1;
 
-    static int[] memo= new int[20];//this will greatly increase speed and avoid kurudia computing the same thing
-    static int factorial(int n){
+    static long[] memo= new long[20];//this will greatly increase speed and avoid kurudia computing the same thing. we also cant use anyu number greater than 20 as we wl requrie big integer or other library for storing largerfactorials
+    static long factorial(int n){
         if (n == 0)
             return 1;
         else{
@@ -22,17 +22,17 @@ public class Main {
     }
 
     //because K = 1n+2m; where K is 20, and n and m are 1 the numbers used
-    public static int forCombinations(int steps){
+    public static long forCombinations(int steps){
         int m=0;
         int n=0;///
         int x=0;
-        int permuts=0;
+        long permuts=0;
         while(n+2*m<=20 && (n*m)-m>=0){
             m = x;
             n = -2*m +20;
             System.out.println(" n "+n+" m "+m +" add "+(n+2*m));
             //compute number of permutations for that combination given by et he formula n!/k!
-            int permut=factorial(20)/factorial(n)*factorial(m);
+            long permut=factorial(20)/factorial(n)*factorial(m);
             System.out.println(" whose permut is "+permut);
             permuts+=permut;
             x++;
