0d94018b7d8f7073bf3e4c1e57e2a68bc2ae566c
warren <shigoliwarren@gmail.com>
2020-11-09 18:10:52 +0300

    the different combination fo the steps

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,13 @@ public class Main {
     final int STEP1=1;
     final int STEP2=1;
 
-
-
-
+    static int[] memo= new int[20];
+    static int factorial(int n){
+        if (n == 0)
+            return 1;
+        else
+            return(n * factorial(n-1));
+    }
 
     //because K = 1n+2m; where K is 20, and n and m are 1 the numbers used
     public static void forCombinations(int steps){
