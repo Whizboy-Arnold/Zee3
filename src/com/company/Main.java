71ca7d191862e2ccbf709439c051719af47a3e4c
warren <shigoliwarren@gmail.com>
2020-11-09 18:26:06 +0300

    AND HERE THE NUMBER IS VERY LARGE FROM DEBUG WE WL NEED TO USE BIG NUMBER ISNTEAD

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,49 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
     final int STEP1=1;
     final int STEP2=1;
 
-    static int[] memo= new int[20];//this will greatly increase speed and avoid redundant computation
+    static int[] memo= new int[20];//this will greatly increase speed and avoid kurudia computing the same thing
     static int factorial(int n){
         if (n == 0)
             return 1;
         else{
-            if(memo[n]>0){
-                return memo[n];
+            if(memo[n-1]>0){
+                return memo[n-1];
             }else{
-                memo[n] = (n * factorial(n-1));
-                return memo[n];
+                memo[n-1] = (n * factorial(n-1));
+                System.out.println(" memo updated to " + Arrays.toString(memo));
+                return memo[n-1];
             }
         }
     }
 
     //because K = 1n+2m; where K is 20, and n and m are 1 the numbers used
-    public static void forCombinations(int steps){
+    public static int forCombinations(int steps){
         int m=0;
         int n=0;///
         int x=0;
+        int permuts=0;
         while(n+2*m<=20 && (n*m)-m>=0){
             m = x;
             n = -2*m +20;
             System.out.println(" n "+n+" m "+m +" add "+(n+2*m));
             //compute number of permutations for that combination given by et he formula n!/k!
-            int permuts=
+            int permut=factorial(20)/factorial(n)*factorial(m);
+            System.out.println(" whose permut is "+permut);
+            permuts+=permut;
             x++;
         }
+        return permuts;
     }
 
 
 
     public static void main(String[] args) {
 	// write your code here
-        forCombinations(20);
+        System.out.println("number of ways : "+forCombinations(20));
     }
 }
